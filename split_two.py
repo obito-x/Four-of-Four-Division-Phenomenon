0666419a10426d20f34ddb5a7b3db78fedc87698
obito-x <53710795+obito-x@users.noreply.github.com>
2021-01-28 21:53:51 +0630

    Update split_two.py

--- split_two.py
+++ split_two.py
@@ -2,20 +2,6 @@
 import sys
 
 
-# 
-#	   +------------+
-#	   | 	   40- 19   =  21 ------------------+
-#	   | 	        |	    |			        |
-#	   | 	(9+10)<-+		+-> (18+19)         |
-#	   | 	  					 			    |
-#      | 	 				 			        |    
-#      | 					    			    |					  
-# [(i, 19-i) for i in range(1,19+1)]	  [(i, 21-i) for i in range(1,21+1)]
-#	   |										|
-#  (if 19-i >= i)					       (if 21-i >= i)
-
-
-
 def split_two(num):
 	for i in range(1, num+1):
 	    test = num - i
